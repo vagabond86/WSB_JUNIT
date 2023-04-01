c42ee842c283f302d9b73fbb03fd5512aab5bf6b
Kamil Górzyński <kamilgorzynski@gmail.com>
2023-04-01 10:44:30 +0200

    init

---
+++ src/main/java/wsb/junit/WsbJunitApplication.java
@@ -0,0 +1,13 @@
+package wsb.junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsbJunitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsbJunitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wsb/junit/WsbJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package wsb.junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsbJunitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
